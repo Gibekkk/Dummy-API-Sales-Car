662a922563022f4a97aa3cb28b6feaf8e3ebff0d
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-25 15:34:25 +0800

    statistik penjualan dummy

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -6,21 +6,37 @@ use App\Http\Controllers\Controller;
 
 class StatisticController extends Controller
 {
-    public function profile()
+    public function generalPenjualan()
     {
-        $return = [
-            "name" => "John Doe",
-            "email" => "example@gmail.com",
-            "nomorWhatsapp" => "08123456789",
-            "perusahaan" => "My Company",
-            "kodeReferal" => "12ab34cd",
-            "tanggalLahir" => date("d-m-Y"),
-            "alamat" => [
-                "address" => "123 Main Street",
-                "provinsi" => "Sulawesi Selatan",
-                "kotaKabupaten" => "Makassar/Somewhere",
-            ],
+        $bulans = [
+            "Januari",
+            "Februari",
+            "Maret",
+            "April",
+            "Mei",
+            "Juni",
+            "Juli",
+            "Agustus",
+            "September",
+            "Oktober",
+            "November",
+            "Desember"
         ];
+        $return = [];
+        foreach($bulans as $bulan){
+            $jumlah = random_int(0, 10);
+            $baru = random_int(0, $jumlah);
+            $jumlah -= $baru;
+            $bekas = random_int(0, $jumlah);
+            $jumlah -= $bekas;
+            $tradeIn = $jumlah;
+            $return[$bulan] = [
+                "Item Terjual" => $baru + $bekas + $tradeIn,
+                "Baru" => $baru,
+                "Bekas" => $bekas,
+                "Trade In" => $tradeIn
+            ];
+        }
         return response()->json($return);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KontakController;
+use App\Http\Controllers\StatisticController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,5 +15,8 @@ Route::prefix('/api/v1')->group(function() {
     });
     Route::prefix('/kontak')->group(function() {
         Route::get('/getGeneral', [KontakController::class, 'general']);
+    });
+    Route::prefix('/statistic')->group(function() {
+        Route::get('/getGeneralPenjualan', [StatisticController::class, 'generalPenjualan']);
     });
 });
