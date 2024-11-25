c308f74a595021020ca5e6a82f66cf11f7088c40
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-25 12:00:55 +0800

    update route api

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-Route::prefix('/api/v1')->group(function() {
-    Route::prefix('/user')->group(function() {
-        Route::get('/profile', [ProfileController::class, 'profile']);
-    });
-});
+
 
 require __DIR__.'/auth.php';
+require __DIR__.'/api.php';
